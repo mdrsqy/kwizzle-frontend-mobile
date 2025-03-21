fcd4f777f798432cb480d3d4e1aa7718646b1512
mdrsqy <maurich.pangkey@gmail.com>
2025-03-21 23:37:33 +0700

    update: final design register page

--- lib/features/authentication/register_page.dart
+++ lib/features/authentication/register_page.dart
@@ -11,6 +11,11 @@ class _RegisterPageState extends State<RegisterPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  final _nameFocusNode = FocusNode();
+  final _usernameFocusNode = FocusNode();
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+
   bool _isPasswordVisible = false; // Track the visibility of the password
 
   void _register() {
@@ -21,18 +26,24 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   @override
+  void dispose() {
+    _nameFocusNode.dispose();
+    _usernameFocusNode.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Color(0xFF6C63FF),
-          ), // Updated to match new color palette
+          icon: Icon(Icons.arrow_back, color: Color(0xFF6C63FF)),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -50,37 +61,47 @@ class _RegisterPageState extends State<RegisterPage> {
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
-                  color: Color(0xFF493D9E), // Unique welcoming message color
+                  color: Color(0xFF493D9E),
                 ),
               ),
-              SizedBox(height: 16), // Increased space between text and form
+              SizedBox(height: 16),
               Text(
                 'Let’s get you started on a fun quiz journey.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Color(0xFF6C63FF), // Color matching the palette
-                ),
+                style: TextStyle(fontSize: 16, color: Color(0xFF6C63FF)),
               ),
-              SizedBox(height: 40), // Increased space before form fields
-              _buildTextField(controller: _nameController, label: 'Full Name'),
+              SizedBox(height: 40),
+              _buildTextField(
+                controller: _nameController,
+                label: 'Full Name',
+                focusNode: _nameFocusNode,
+                nextFocusNode: _usernameFocusNode,
+              ),
               SizedBox(height: 12),
               _buildTextField(
                 controller: _usernameController,
                 label: 'Username',
+                focusNode: _usernameFocusNode,
+                nextFocusNode: _emailFocusNode,
               ),
               SizedBox(height: 12),
-              _buildTextField(controller: _emailController, label: 'Email'),
+              _buildTextField(
+                controller: _emailController,
+                label: 'Email',
+                focusNode: _emailFocusNode,
+                nextFocusNode: _passwordFocusNode,
+              ),
               SizedBox(height: 12),
               _buildTextField(
                 controller: _passwordController,
                 label: 'Password',
                 obscureText: !_isPasswordVisible,
+                focusNode: _passwordFocusNode,
                 suffixIcon: IconButton(
                   icon: Icon(
                     _isPasswordVisible
                         ? Icons.visibility
                         : Icons.visibility_off,
-                    color: Color(0xFF6C63FF), // Updated icon color
+                    color: Color(0xFF6C63FF),
                   ),
                   onPressed: () {
                     setState(() {
@@ -93,9 +114,7 @@ class _RegisterPageState extends State<RegisterPage> {
               ElevatedButton(
                 onPressed: _register,
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(
-                    0xFF6C63FF,
-                  ), // Updated to match new color palette
+                  backgroundColor: Color(0xFF6C63FF),
                   padding: EdgeInsets.symmetric(horizontal: 40, vertical: 14),
                   minimumSize: Size(double.infinity, 48),
                   shape: RoundedRectangleBorder(
@@ -114,9 +133,7 @@ class _RegisterPageState extends State<RegisterPage> {
                 },
                 child: Text(
                   "Already have an account? Sign In.",
-                  style: TextStyle(
-                    color: Color(0xFF6C63FF),
-                  ), // Updated to match new color palette
+                  style: TextStyle(color: Color(0xFF6C63FF)),
                 ),
               ),
             ],
@@ -131,24 +148,32 @@ class _RegisterPageState extends State<RegisterPage> {
     required TextEditingController controller,
     required String label,
     bool obscureText = false,
+    required FocusNode focusNode,
+    FocusNode? nextFocusNode,
     Widget? suffixIcon,
   }) {
     return TextField(
       controller: controller,
+      focusNode: focusNode,
       obscureText: obscureText,
       decoration: InputDecoration(
         labelText: label,
-        labelStyle: TextStyle(color: Colors.black), // Keep label text black
+        labelStyle: TextStyle(color: Colors.black),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Color(0xFF6C63FF),
-          ), // Updated focused border color
+          borderSide: BorderSide(color: Color(0xFF6C63FF)),
         ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
         suffixIcon: suffixIcon,
       ),
+      textInputAction:
+          nextFocusNode != null ? TextInputAction.next : TextInputAction.done,
+      onEditingComplete: () {
+        if (nextFocusNode != null) {
+          FocusScope.of(context).requestFocus(nextFocusNode);
+        } else {
+          FocusScope.of(context).unfocus();
+        }
+      },
     );
   }
 }
