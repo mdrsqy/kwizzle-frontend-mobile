05bc6a9c3ae1e89e7b9fd69a5721e0f1af1733d9
mdrsqy <maurich.pangkey@gmail.com>
2025-03-22 10:37:41 +0700

    update: register page to backend

--- lib/features/authentication/register_page.dart
+++ lib/features/authentication/register_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // Added import
 
 class RegisterPage extends StatefulWidget {
   @override
